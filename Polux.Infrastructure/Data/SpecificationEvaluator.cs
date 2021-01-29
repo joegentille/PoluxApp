28839dbd3b0cb171d28b03d8a3c62cfbca22a3ef
joegentille <joegentille@gmail.com>
2021-01-29 00:06:16 -0500

    Added Specification Pattern

---
+++ Polux.Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Polux.Core.Entities;
+using Polux.Core.Specifications;
+using System.Linq;
+
+namespace Polux.Infrastructure.Data
+{
+    public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
+        {
+            var query = inputQuery;
+            if(spec.Criteria != null)
+            {
+                query = query.Where(spec.Criteria);
+            }
+
+            query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
+            return query;
+        }
+    }
+}
