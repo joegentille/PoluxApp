12d184549ea75234a4ded253c60e6a338a3e74fc
joegentille <joegentille@gmail.com>
2021-02-17 14:22:26 -0500

    Some house cleaning to startup class

--- Polux.API/Startup.cs
+++ Polux.API/Startup.cs
@@ -1,17 +1,13 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
-using Polux.API.Errors;
+using Polux.API.Extensions;
 using Polux.API.Helpers;
 using Polux.API.Middleware;
-using Polux.Core.Interfaces;
 using Polux.Infrastructure.Data;
-using System.Linq;
 
 namespace Polux.API
 {
@@ -26,40 +22,14 @@ namespace Polux.API
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        {            
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
+            services.AddApplicationServices();
             services.AddDbContext<StoreContext>(opt =>
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
             );
-
-            // Para modificar el comportamiento de ApiController
-            // Probar este request https://localhost:5001/api/buggy/badrequest/five
-            // sin la configuracion de abajo para ver la diferencia de respuestas
-            services.Configure<ApiBehaviorOptions>(options => 
-            {
-                options.InvalidModelStateResponseFactory = actionContext =>
-                {
-                    var errors = actionContext.ModelState
-                        .Where(e => e.Value.Errors.Count > 0)
-                        .SelectMany(x => x.Value.Errors)
-                        .Select(x => x.ErrorMessage).ToArray();
-
-                    var errorResponse = new ApiValidationErrorResponse
-                    {
-                        Errors = errors
-                    };
-
-                    return new BadRequestObjectResult(errorResponse);
-                };
-            });
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Skinet API", Version = "v1" });
-            });
+            services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -78,12 +48,7 @@ namespace Polux.API
 
             app.UseAuthorization();
 
-            app.UseSwagger();
-
-            app.UseSwaggerUI(c => 
-            { 
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkiNet API v1"); 
-            });
+            app.UseSwaggerDocumentation();
 
             app.UseEndpoints(endpoints =>
             {
