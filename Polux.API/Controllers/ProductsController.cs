8a7f399810a544257b4671fc96701d07aa6d2ede
joegentille <joegentille@gmail.com>
2021-02-17 18:32:23 -0500

    Added filtering functionality

--- Polux.API/Controllers/ProductsController.cs
+++ Polux.API/Controllers/ProductsController.cs
@@ -27,9 +27,9 @@ namespace Polux.API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string sort)
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string sort, int? brandId, int? typeId)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification(sort);
+            var spec = new ProductsWithTypesAndBrandsSpecification(sort, brandId, typeId);
             var products = await _productsRepo.ListAsync(spec);
             return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
         }
