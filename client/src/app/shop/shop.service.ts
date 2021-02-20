7d43f16b87911e88d11b9eccffd5c263b0eed9bf
joegentille <joegentille@gmail.com>
2021-02-20 16:34:56 -0500

    Search functionality added

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -27,6 +27,11 @@ export class ShopService {
       params = params.append('typeId', shopParams.typeId.toString());
     }
 
+    if(shopParams.search)
+    {
+      params = params.append('search', shopParams.search);
+    }
+
     params = params.append('sort', shopParams.sort);
 
     params = params.append('pageIndex', shopParams.pageNumber.toString());
