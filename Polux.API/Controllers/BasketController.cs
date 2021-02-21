0cd2f0c569799c5f5534c525e8b8b72f41a95c0d
joegentille <joegentille@gmail.com>
2021-02-21 18:33:36 -0500

    Created the BasketController to handle client's requests

---
+++ Polux.API/Controllers/BasketController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Polux.Core.Entities;
+using Polux.Core.Interfaces;
+using System.Threading.Tasks;
+
+namespace Polux.API.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CustomerBasket>> GetBasketById(string id)
+        {
+            var basket = await _basketRepository.GetBasketAsync(id);
+
+            return Ok(basket ?? new CustomerBasket(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        {
+            var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+
+            return Ok(updatedBasket);
+        }
+
+        [HttpDelete]
+        public async Task DeleteBasketAsync(string id)
+        {
+            await _basketRepository.DeleteBasketAsync(id);
+        }
+    }
+}
