6ccff9a966384010f12a455ad954e923399c1835
joegentille <joegentille@gmail.com>
2021-02-20 20:39:01 -0500

    Modified the code to make shop lazy loading

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -4,8 +4,7 @@ import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { RouterModule } from '@angular/router';
-
+import { ShopRoutingModule } from './shop-routing.module';
 
 
 @NgModule({
@@ -13,8 +12,7 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule
-  ],
-  exports: [ShopComponent]
+    ShopRoutingModule
+  ]
 })
 export class ShopModule { }
