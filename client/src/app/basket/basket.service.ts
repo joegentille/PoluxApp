bcfe3ceb7e9b57cf139c0409cb64a35695f734f7
joegentille <joegentille@gmail.com>
2021-02-22 00:04:20 -0500

    Worked on the calculation of totals for the basket items

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -5,7 +5,7 @@ import { BehaviorSubject } from 'rxjs';
 import { Basket, IBasket } from '../shared/models/Basket';
 import { map } from 'rxjs/operators';
 import { IProduct } from '../shared/models/product';
-import { IBasketItem } from '../shared/models/basket';
+import { IBasketItem, IBasketTotals } from '../shared/models/basket';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +14,8 @@ export class BasketService {
   baseUrl = environment.apiUrl;
   private basketSource = new BehaviorSubject<IBasket>(null);
   basket$ = this.basketSource.asObservable();
+  private basketTotalSource = new BehaviorSubject<IBasketTotals>(null);
+  basketTotal$ = this.basketTotalSource.asObservable();
 
   constructor(private http: HttpClient) { }
 
@@ -22,7 +24,7 @@ export class BasketService {
       .pipe(
         map((basket: IBasket) => {
           this.basketSource.next(basket);
-          console.log(this.getCurrentBasketValue());
+          this.calculateTotals();
         })
       )
   }
@@ -30,7 +32,7 @@ export class BasketService {
   setBasket(basket: IBasket) {
     return this.http.post(this.baseUrl + 'basket', basket).subscribe((response: IBasket) => {
       this.basketSource.next(response);
-      console.log(response);
+      this.calculateTotals();
     }, error => {
       console.log(error);
     })
@@ -49,6 +51,14 @@ export class BasketService {
     }
     basket.items = this.addOrUpdateItem(basket.items, itemToAdd, quantity);
     this.setBasket(basket);
+  }
+
+  private calculateTotals() {
+    const basket = this.getCurrentBasketValue();
+    const shipping = 0;
+    const subtotal = basket.items.reduce((a, b) => (b.price * b.quantity) + a, 0);
+    const total = subtotal + shipping;
+    this.basketTotalSource.next({shipping, total, subtotal});
   }
 
   private addOrUpdateItem(items: IBasketItem[], itemToAdd: IBasketItem, quantity: number): IBasketItem[] {
