68af11195703ae312598ae526af7611887109daa
joegentille <joegentille@gmail.com>
2021-02-23 18:41:06 -0500

    Angular - Added login functionality and persisting the login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BasketService } from './basket/basket.service';
+import { AccountService } from './account/account.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,11 +9,16 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'Joe';
 
-  constructor(private basketService: BasketService)
+  constructor(private basketService: BasketService, private accountService: AccountService)
   {
   }
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }  
+  
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if(basketId) {
       this.basketService.getBasket(basketId).subscribe(() => {
@@ -21,6 +27,17 @@ export class AppComponent implements OnInit {
         console.log(error);
       });
     }
-  }  
-  
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.accountService.loadCurrentUser(token).subscribe(() => {
+        console.log('loaded user');
+      }, error => {
+        console.log(error);
+      })
+    }
+  }
+
 }
--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { BasketService } from '../../basket/basket.service';
 import { Observable } from 'rxjs';
 import { IBasket } from 'src/app/shared/models/Basket';
+import { IUser } from 'src/app/shared/models/user';
+import { AccountService } from 'src/app/account/account.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,12 +11,15 @@ import { IBasket } from 'src/app/shared/models/Basket';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
-
-  constructor(private basketService: BasketService) { }
   basket$: Observable<IBasket>;
+  currentUser$: Observable<IUser>;
+
+  constructor(private basketService: BasketService, private accountService: AccountService) { }
+  
 
   ngOnInit(): void {
     this.basket$ = this.basketService.basket$;
+    this.currentUser$ = this.accountService.currentUser$;
   }
 
 }
