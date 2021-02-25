b1de6f82b0bf17c16520a37e4ca9b3a620e630eb
joegentille <joegentille@gmail.com>
2021-02-25 18:58:30 -0500

    Angular - Updated the DeliveryMethod to calculate the totals

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -6,6 +6,7 @@ import { Basket, IBasket } from '../shared/models/Basket';
 import { map } from 'rxjs/operators';
 import { IProduct } from '../shared/models/product';
 import { IBasketItem, IBasketTotals } from '../shared/models/basket';
+import { IDeliveryMethod } from '../shared/models/deliveryMethod';
 
 @Injectable({
   providedIn: 'root'
@@ -16,8 +17,14 @@ export class BasketService {
   basket$ = this.basketSource.asObservable();
   private basketTotalSource = new BehaviorSubject<IBasketTotals>(null);
   basketTotal$ = this.basketTotalSource.asObservable();
+  shipping = 0;
 
   constructor(private http: HttpClient) { }
+
+  setShippingPrice(deliveryMethod: IDeliveryMethod) {
+    this.shipping = deliveryMethod.price;
+    this.calculateTotals();
+  }
 
   getBasket(id: string) {
     return this.http.get(this.baseUrl + 'basket?id=' + id)
@@ -95,7 +102,7 @@ export class BasketService {
 
   private calculateTotals() {
     const basket = this.getCurrentBasketValue();
-    const shipping = 0;
+    const shipping = this.shipping;
     const subtotal = basket.items.reduce((a, b) => (b.price * b.quantity) + a, 0);
     const total = subtotal + shipping;
     this.basketTotalSource.next({shipping, total, subtotal});
